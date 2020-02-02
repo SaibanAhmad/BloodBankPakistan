4b52d8b0b4f1821900f9fc0021d1626c578a8139
saibanahmadcs@gmail.com <saibanahmadcs@gmail.com>
2020-02-03 03:11:22 +0500

    SplashScreen and Logos Added

--- app/src/main/java/com/example/bloodbankpakistan/SplashScreen.java
+++ app/src/main/java/com/example/bloodbankpakistan/SplashScreen.java
@@ -2,6 +2,7 @@ package com.example.bloodbankpakistan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        background.start();
     }
 }
